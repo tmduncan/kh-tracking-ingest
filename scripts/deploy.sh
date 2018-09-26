7b447060bf2dc4d99895afde88aa0e79ceecd0aa
tduncan <tmduncan@gmail.com>
2018-09-26 00:48:43 -0500

    Region params Dynamic bucket prefix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/env
 
+aws s3 ls s3://${bucket} > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    aws s3 mb s3://${bucket}
+fi
+
 sam package \
+    --region ${region} \
     --template-file $template \
-    --s3-bucket $bucket \
-    --output-template-file $output
+    --s3-bucket ${bucket} \
+    --output-template-file ${output}
 
 sam deploy \
-    --template-file $output \
-    --stack-name $stack \
+    --region ${region} \
+    --template-file ${output} \
+    --stack-name ${stack} \
     --capabilities CAPABILITY_IAM $@
--- scripts/track.sh
+++ scripts/track.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ${DIR}/env
 
-url=$(aws cloudformation describe-stacks --stack-name $stack --query Stacks[0].Outputs[0].OutputValue --output text)
+url=$(aws cloudformation describe-stacks --region ${region} --stack-name $stack --query Stacks[0].Outputs[0].OutputValue --output text)
 record='{"name": "John", "action": "charge", "value": 100}'
 
 curl \
